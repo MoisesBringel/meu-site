88b216561dc9a63b8de1fc5121eb585b7f143175
MoisesBringel <bmouzer@gmail.com>
2023-07-25 21:38:11 -0300

    tres arquivos js

---
+++ operadores.js
@@ -0,0 +1,67 @@
+//Aritméticos
+// + adição
+// - subtração
+// * multiplicação
+// / divisão
+// % modulo (resto)
+let numeroUm = 5
+let numeroDois = 6
+let numeroTres = -1
+
+const mensagem = `NumeroDois (${numeroDois}) + NumeroTres (${numeroTres}) = ${numeroDois+numeroTres}`
+
+console.log(mensagem)
+
+console.log(numeroDois+"+"+numeroTres+"="+(numeroDois+numeroTres))
+
+console.log(numeroDois%numeroUm);
+//Relacionais
+// > maior que
+// < menor que
+// != diferente
+//     !==
+// == igual
+//     ===
+// >= maior ou igual
+// <= menor ou igual
+
+let numeroString = '6'
+console.log(numeroDois === numeroString)
+// = Atribuição
+
+//Lógicos
+// && E
+// || OU
+// ! NÃO
+
+console.log(!(numeroDois === numeroString))
+
+//autoincremento
+let a = 0;
+
+a = a + 1;
+console.log(a);
+
+a++;
+console.log(a);
+
+++a;
+console.log(a);
+
+let b = 10;
+
+console.log(b--);
+
+console.log(--b);
+
+//aritméticos de atribuição
+// +=
+// -+
+// *=
+// /=
+// %=
+let c = 0;
+c = c + 5;
+console.log(c);
+c += 5;
+console.log(c);
---
+++ variaveis.js
@@ -0,0 +1,18 @@
+//usando var ou nada
+// identificador
+//     atribuicao
+// valor
+//possível erro com undefined
+var nome="Érico"
+sobrenome="Costa"
+console.log(nome)
+
+//não permite a execução antes da criação (ERRO)
+let nome2 = 'Senac'
+console.log(typeof nome2)
+
+const pi=3.141923
+console.log(typeof pi)
+
+const mensagem = `O nome é: ${nome} e sobrenome é: ${sobrenome}`
+console.log(mensagem)
